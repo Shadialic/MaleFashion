ca49cdef7321b9ecd46fb1f9b0b2457f2815fae6
Shadilalic <shadilsa786@gmai.com>
2023-08-25 12:44:59 +0530

    fivth commit

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const mongoose = require('mongoose');
 mongoose.connect("mongodb://127.0.0.1:27017/userdb");
-const bodyParser = require('body-parser');
-const nocache = require('nocache')
 const expressLayouts = require("express-ejs-layouts")
 const app = express();
 
 const userRouter = require('./routes/userRoutes');
 const adminRoutes = require('./routes/adminRoutes');
-const address_route = require('./routes/adressRoute')
 
-app.use("/", userRouter)
-app.use('/admin', adminRoutes)
+app.use("/", userRouter);
+app.use('/admin', adminRoutes);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
